0c8440da738ebb1213aae80b563a75482072055a
Shawn Melton <wshawnmelton@gmail.com>
2017-05-25 14:48:30 -0500

    adding task to run pester test, add tag to initial describe

--- test/pythian-sqlserver.Tests.ps1
+++ test/pythian-sqlserver.Tests.ps1
@@ -1,7 +1,7 @@
 $ModuleManifestName = 'pythian-sqlserver.psd1'
 $ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
 
-Describe 'Module Manifest Tests' {
+Describe 'Module Manifest Tests' -Tags "module-base" {
     It 'Passes Test-ModuleManifest' {
         Test-ModuleManifest -Path $ModuleManifestPath
         $? | Should Be $true
